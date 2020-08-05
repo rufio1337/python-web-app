b29753295e7ebcae86b9908544393f0dca59bc59
Zachary Dahle <rufio1337@gmail.com>
2020-08-05 18:46:28 +0000

    Create initial models and migrate database. (Fixed spelling error in config.)

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ db_password = os.environ.get('DB_PASSWORD', default='')
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABSE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
